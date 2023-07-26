2dbe359a0e1edfb90f9fe511d09b5ec0f7cafe30
Dan Goldwater <dangoldwater@gmail.com>
2023-07-26 15:54:05 +0100

    Started vectorstor

---
+++ src/vector_store/vector_store.py
@@ -0,0 +1,15 @@
+from langchain.document_loaders import TextLoader
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import DirectoryLoader
+import pathlib
+
+PATH_TO_DATA = pathlib.Path('data/feedback_data/')
+loader = DirectoryLoader(PATH_TO_DATA, glob="**/*.odt")
+
+# Load the document, split it into chunks, embed each chunk and load it into the vector store.
+raw_documents = TextLoader('../../../state_of_the_union.txt').load()
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+documents = text_splitter.split_documents(raw_documents)
+db = Chroma.from_documents(documents, OpenAIEmbeddings())
