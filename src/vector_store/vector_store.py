5f4e2dd799a585c5d9e045aa78216e650cfba91b
Dan Goldwater <dangoldwater@gmail.com>
2023-07-26 16:02:42 +0100

    added vectorstore

--- src/vector_store/vector_store.py
+++ src/vector_store/vector_store.py
@@ -6,10 +6,11 @@ from langchain.document_loaders import DirectoryLoader
 import pathlib
 
 PATH_TO_DATA = pathlib.Path('data/feedback_data/')
-loader = DirectoryLoader(PATH_TO_DATA, glob="**/*.odt")
+loader = DirectoryLoader(PATH_TO_DATA, glob="**/*.odt", use_multithreading=True)
 
 # Load the document, split it into chunks, embed each chunk and load it into the vector store.
-raw_documents = TextLoader('../../../state_of_the_union.txt').load()
+# raw_documents = TextLoader('../../../state_of_the_union.txt').load()
+raw_documents = loader.load()
 text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 documents = text_splitter.split_documents(raw_documents)
 db = Chroma.from_documents(documents, OpenAIEmbeddings())
