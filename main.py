5ce386af741787c6f2cea5abc5f85083fa470cee
Dan Goldwater <dangoldwater@gmail.com>
2023-07-26 15:29:09 +0100

    added data

---
+++ main.py
@@ -0,0 +1,36 @@
+import langchain
+import dotenv
+import openai
+from langchain.llms import OpenAI
+from langchain import PromptTemplate, LLMChain
+
+# from dotenv import load_dotenv, find_dotenv
+
+# load_dotenv(find_dotenv())
+
+import os
+key = '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'
+os.environ["OPENAI_API_KEY"] = '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'
+
+# g4 = OpenAI(model='gpt-4')
+
+# g4('What is a horse')
+llm = OpenAI(openai_api_key=key)
+
+template = """Question: {question}
+
+Answer: Let's think step by step."""
+
+prompt = PromptTemplate(template=template, input_variables=["question"])
+
+llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+completion = openai.ChatCompletion.create( # Change the function Completion to ChatCompletion
+  model = 'gpt-3.5-turbo',
+  messages = [ # Change the prompt parameter to the messages parameter
+    {'role': 'user', 'content': 'What is a horse?'}
+  ],
+  temperature = 0  
+)
+
+print(completion['choices'][0]['message']['content'])
