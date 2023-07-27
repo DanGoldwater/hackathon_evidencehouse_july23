59fc5554ae3afdc0298dadfcc6fff55b15d3b131
henry8th <jessedeng97@gmail.com>
2023-07-27 12:43:25 +0100

    updated risk factor ui

--- src/ui/components/risk_factors_tab.py
+++ src/ui/components/risk_factors_tab.py
@@ -3,8 +3,6 @@ import gradio as gr
 
 def risk_factors_tab_ui(risk_factors: list[str]):
     with gr.Column():
-        for factor in risk_factors:
-            gr.Label(factor)
-    # gr.HighlightedText(
-    #     value=[("Text", "Label 1"), ("to be", "Label 2"), ("highlighted", "Label 3")]
-    # )
+        gr.TextArea(
+            value="\n\n".join(risk_factors), min_lines=4, max_lines=30, readonly=True
+        )
