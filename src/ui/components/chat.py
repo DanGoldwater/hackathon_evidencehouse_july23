1a1089db0bd31ea586f7971c1f629b8ca69995a7
henry8th <jessedeng97@gmail.com>
2023-07-28 12:03:56 +0100

    charts and summary

--- src/ui/components/chat.py
+++ src/ui/components/chat.py
@@ -2,6 +2,10 @@ import time
 import random
 import openai
 import gradio as gr
+from src.ui.components.graphs_tab import (
+    create_stacked_barchart_df,
+    get_unforseen_costs_html,
+)
 from src.ui.components.cost_drivers_tab import (
     get_cost_driver_html_single,
 )
@@ -19,6 +23,7 @@ from src.ui.backend.llm_inference import (
 from src.ui.components.risk_factors_tab import get_risk_factor_html_total
 from src.ui.components.cost_drivers_tab import get_cost_driver_html_total
 from src.vector_store import vector_store
+from typing import Tuple
 
 TOP_K_SIMILAR_CONTRACTS = 5
 
@@ -26,6 +31,10 @@ TOP_K_SIMILAR_CONTRACTS = 5
 def chat_ui(
     risk_factors: gr.Blocks,
     cost_drivers: gr.Blocks,
+    markdown_summary: gr.Blocks,
+    barplot1: gr.Blocks,
+    barplot2: gr.Blocks,
+    unforseen_costs_html: gr.Blocks,
 ):
     chatbot = gr.Chatbot()
     msg = gr.Textbox()
@@ -71,23 +80,74 @@ def chat_ui(
             history[-1][1] += character
             yield history
 
-    def update_charts(history, risk_factors, cost_drivers):
+    def update_charts(
+        history,
+        risk_factors,
+        cost_drivers,
+        markdown_summary,
+        barplot1,
+        barplot2,
+        unforseen_costs,
+    ):
         if len(history) == 1:
+            # summary
+            summary = "Summary: " + history[-1][1].split("Summary: ")[-1]
+            # bar plots (recomput)
+            sub_df = vector_store.query_vector_store(
+                df=vector_store.DF,
+                path=vector_store.FAISS_OPAI_PATH,
+                query_text=history[-1][0],
+            )
+            sub_df = embellish.embellish_dataframe(df=sub_df)
+            barplot_1 = sub_df
+            barplot_2 = create_stacked_barchart_df(sub_df)
+            unforseen_costs = get_unforseen_costs_html(sub_df)
+
+            # data
             risk_factors_data = get_risk_factor_html_total(
                 parse_risk_factors_data(history[-1][1])
             )
             cost_drivers_data = get_cost_driver_html_total(
                 parse_cost_drivers_data(history[-1][1])
             )
-            return risk_factors_data, cost_drivers_data
+            return (
+                risk_factors_data,
+                cost_drivers_data,
+                summary,
+                barplot_1,
+                barplot_2,
+                unforseen_costs,
+            )
         else:
-            return risk_factors, cost_drivers
+            return (
+                risk_factors,
+                cost_drivers,
+                markdown_summary,
+                barplot1,
+                barplot2,
+                unforseen_costs,
+            )
 
     msg.submit(user, [msg, chatbot], [msg, chatbot], queue=False).then(
         bot, chatbot, chatbot
     ).then(
         update_charts,
-        [chatbot, risk_factors, cost_drivers],
-        [risk_factors, cost_drivers],
+        [
+            chatbot,
+            risk_factors,
+            cost_drivers,
+            markdown_summary,
+            barplot1,
+            barplot2,
+            unforseen_costs_html,
+        ],
+        [
+            risk_factors,
+            cost_drivers,
+            markdown_summary,
+            barplot1,
+            barplot2,
+            unforseen_costs_html,
+        ],
     )
     clear.click(lambda: None, None, chatbot, queue=False)
--- src/ui/components/graphs_tab.py
+++ src/ui/components/graphs_tab.py
@@ -2,36 +2,71 @@ import gradio as gr
 import pandas as pd
 
 
-# TODO - a hack!
-dummy_data = pd.DataFrame({"Notice Identifier": [], "Estimate - Labour: A-AAA":[], "Estimate - Labour: B - BBB":[], "Estimate - Overhead: A-AAA":[], "Estimate - Overhead: B - BBB":[], 'Estimate - Materials: A-AAA':[], 'Estimate - Materials: B - BBB':[], "Total Estimate": [], "unforseen_costs": []})
-
-dummy_data2 = pd.read_csv("src/data/test_chart_data2.csv") # For a sample project - air conditioning in hospital
-
-
-def costs_barchart_ui(df):
+def create_stacked_barchart_df(raw_df):
     id_col = "Notice Identifier"
-    categories = ["Estimate - Labour: A-AAA", "Estimate - Labour: B - BBB", "Estimate - Overhead: A-AAA", "Estimate - Overhead: B - BBB", 'Estimate - Materials: A-AAA', 'Estimate - Materials: B - BBB']
-    categories_and_id = [id_col, "Total Estimate", "Estimate - Labour: A-AAA", "Estimate - Labour: B - BBB", "Estimate - Overhead: A-AAA", "Estimate - Overhead: B - BBB", 'Estimate - Materials: A-AAA', 'Estimate - Materials: B - BBB', "unforseen_costs"]
-    
-    df = df[categories_and_id]
-
-    # Total costs bar chart
-    total_costs_df = df[[id_col, "Total Estimate"]] # TODO check column names including 
-    gr.BarPlot(total_costs_df, x=id_col, y="Total Estimate", title="Estimated costs of similar projects", min_width=1200)
-
-    # Stacked 
-    stacked_df = df[categories_and_id]
+    categories_and_id = [
+        id_col,
+        "Total Estimate",
+        "Estimate - Labour: A-AAA",
+        "Estimate - Labour: B - BBB",
+        "Estimate - Overhead: A-AAA",
+        "Estimate - Overhead: B - BBB",
+        "Estimate - Materials: A-AAA",
+        "Estimate - Materials: B - BBB",
+        "unforseen_costs",
+    ]
+    categories = [
+        "Estimate - Labour: A-AAA",
+        "Estimate - Labour: B - BBB",
+        "Estimate - Overhead: A-AAA",
+        "Estimate - Overhead: B - BBB",
+        "Estimate - Materials: A-AAA",
+        "Estimate - Materials: B - BBB",
+    ]
+    stacked_df = raw_df[categories_and_id]
     stacked_df = stacked_df.melt(id_vars=id_col, value_vars=categories)
-    gr.BarPlot(stacked_df, x=id_col, y="value", color="variable", title="Estimated costs split by type")
+    return stacked_df
+
+
+def get_unforseen_costs_list(raw_df):
+    return raw_df["unforseen_costs"].values.tolist()
+
+
+def get_unforseen_costs_html(df=None):
+    html = ""
+    if df is None:
+        return html
+    unforseen_costs = df["unforseen_costs"].values.tolist()
+    for description in unforseen_costs:
+        html += f"<li>{description}</li>"
+    return html
+
+
+def costs_barchart_ui():
+    # Total costs bar chart
+    barplot1 = gr.BarPlot(
+        # df,
+        x="Notice Identifier",
+        y="Total Estimate",
+        title="Estimated costs of similar projects",
+        min_width=1200,
+    )
+
+    barplot2 = gr.BarPlot(
+        # create_stacked_barchart_df(df),
+        x="Notice Identifier",
+        y="value",
+        color="variable",
+        title="Estimated costs split by type",
+    )
 
     # Unforseen costs
     gr.HTML("<b>Unforseen costs</b>")
     with gr.Column():
-        unforseen_costs = df["unforseen_costs"].values.tolist()
-        for description in unforseen_costs:
-            gr.HTML(f"<li>{description}</li>")
+        html = get_unforseen_costs_html(None)
+    returned_html = gr.HTML(html)
+    return barplot1, barplot2, returned_html
 
 
-def graphs_tab_ui(df=dummy_data2):
-
-    return costs_barchart_ui(df)
+def graphs_tab_ui():
+    return costs_barchart_ui()
